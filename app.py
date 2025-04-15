8a7431c042d95acea9ca4e1b895b7c755c667d11
shubham <navneetsingh94520@gmail.com>
2025-04-15 20:49:43 +0530

    Day 2: Added HTML template and connected to Flask

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/hit')
+def hit():
+    return "You got hit by a random Character!"
 
 if __name__ == '__main__':
     app.run(debug=True)
